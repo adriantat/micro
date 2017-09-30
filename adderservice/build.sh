40e802d37049fc12774d0fd74e7a455665ce26aa
chanyuntat <chanyuntat@outlook.com>
2017-09-30 17:15:39 +0800

    built script

--- adderservice/build.sh
+++ adderservice/build.sh
@@ -4,20 +4,24 @@ source ~/.bashrc
 GITSHA=$(git rev-parse --short HEAD)
 
 case "$1" in
-  container)
-    sudo -u pelger docker build -t adderservice:$GITSHA .
-    sudo -u pelger docker tag adderservice:$GITSHA pelger/adderservice:$GITSHA 
-    sudo -i -u pelger docker push pelger/adderservice:$GITSHA 
-  ;;
-  deploy)
-    sed -e s/_NAME_/adderservice/ -e s/_PORT_/8080/  < ../deployment/service-template.yml > svc.yml
-    sed -e s/_NAME_/adderservice/ -e s/_PORT_/8080/ -e s/_IMAGE_/pelger\\/adderservice:$GITSHA/ < ../deployment/deployment-template.yml > dep.yml
-    sudo -i -u pelger kubectl apply -f $(pwd)/svc.yml
-    sudo -i -u pelger kubectl apply -f $(pwd)/dep.yml
-  ;;
-  *)
-    echo 'invalid build command'
-    exit 1
-  ;;
+ container)
+   sudo -u adrian docker build -t adderservice:$GITSHA .
+   sudo -u adrian docker tag adderservice:$GITSHA \
+     ytchan17/adderservice:$GITSHA
+   sudo -i -u adrian docker push \
+     ytchan17/adderservice:$GITSHA
+ ;;
+ deploy)
+   sed -e s/_NAME_/adderservice/ -e s/_PORT_/8080/ \
+     < ../deployment/service-template.yml > svc.yml
+   sed -e s/_NAME_/adderservice/ -e s/_PORT_/8080/ \
+     -e s/_IMAGE_/ytchan17\\/adderservice:$GITSHA/ \
+     < ../deployment/deployment-template.yml > dep.yml
+   sudo -i -u ytchan17 kubectl apply -f $(pwd)/svc.yml
+   sudo -i -u ytchan17 kubectl apply -f $(pwd)/dep.yml
+ ;;
+ *)
+   echo invalid build step
+   exit 1
+ ;;
 esac
-
